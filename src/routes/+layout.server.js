004f7789ad4bb0a822b6c06e0fa8ff1ac699e59c
Olakunle Dacosta <dacosta00736@gmail.com>
2026-07-05 04:52:59 +0100

    Fix: Added pre-render rules to the +layout.server.js to skip pre-rendering during build time, and resolve pages dynamically in runtime

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,8 @@
+export const prerender = false;
+export const ssr = true; // Kept true for dynamic Server-Side Rendering
+
+
+/** @type {import('./$types').LayoutServerLoad} */
+/* export async function load() {
+    return {};
+} */
